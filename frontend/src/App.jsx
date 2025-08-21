073b76c8976cdd6a2998b159a4fce00eb18ecff1
Tylerbeyer <Tyler.beyer@gmail.com>
2025-08-21 00:05:10 -0600

    login page working to admin

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,23 @@
 import React, { useContext } from 'react';
 import { AuthContext } from './context/AuthContext';
 import Login from './components/Login';
-import './App.css';
+import Layout from './components/Layout';
 
 function App() {
-  const { user, token, logout, loading } = useContext(AuthContext);
-
-  // Show a loading state while the app is checking for a token
-  if (loading) {
-    return <div>Loading application...</div>;
-  }
+  const { user } = useAuth();
 
   return (
-    <div className="App">
-      {user && token ? (
-        <div>
-          <h1>Welcome, {user.name}!</h1>
-          <p>You are logged in.</p>
-          <button onClick={logout}>Logout</button>
-          {/* The main application dashboard will go here in the future */}
-        </div>
+    <div>
+      {user ? (
+        <Layout>
+          {/* Dashboard content will be added here later */}
+        </Layout>
       ) : (
         <Login />
       )}
     </div>
   );
 }
+
 
 export default App;
